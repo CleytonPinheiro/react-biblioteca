446b92dd85f7e988a06db32b9155853a60a2d175
CleytonPinheiro <cleyton_pin@hotmail.com>
2020-08-23 22:22:29 -0300

    Passando propriedades.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 import ListaDeNotas from "./components/ListaDeNotas/ListaDeNotas"
 import FormularioCadastro from "./components/FormularioCadastro/formularioCadastro";
-
 import "./assets/App.css";
 import './assets/index.css';
 
 class App extends Component {
+
+    criarNota(titulo, texto) {
+
+    }
+
     render() {
         return (
             <section className="conteudo">
 
-                <FormularioCadastro />
+                <FormularioCadastro criarNota={this.criarNota}/>
                 <ListaDeNotas />
 
             </section>
